c35610065e7c2ed49ee5d3eb232380da505f919f
BriranSus <alexander.susanto001@binus.ac.id>
2025-08-07 17:51:16 +0700

    Added glowing effect to button

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -19,7 +19,7 @@ function LandingPage() {
                         <img src={MoodiFYLogo} className="size-[104px] z-10 mb-8" alt="" />
                         <p className="text-[84px] text-Semi_bright_red z-10 font-bold">Welcome to MoodiFY</p>    
                         <p className="text-[36px] text-Very_bright_red z-10 font-bold">Music That Matches your Mood</p>
-                        <button onClick={startedNavigation} className="cursor-pointer text-[36px] text-Bright_red font-bold z-20 bg-Dark_red px-18 py-6 mt-12 border border-Red border-3 rounded-[24px]">
+                        <button onClick={startedNavigation} className="cursor-pointer text-[36px] text-Bright_red font-bold z-20 bg-Dark_red px-18 py-6 mt-12 border border-Red border-3 rounded-[24px] hover:shadow-[0px_0px_34.7px_rgba(242,97,63,1)] hover:transition-all ease-in-out duration-200">
                             Get Started
                         </button>
                     </div>
