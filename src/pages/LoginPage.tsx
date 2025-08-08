75336a2e9d7e3968a90a233bb27f63bc369dcc14
BriranSus <alexander.susanto001@binus.ac.id>
2025-08-08 10:52:09 +0700

    Added Login Card

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,15 @@ function LoginPage () {
     return (
         <>
             <Navbar currentPage={"Login"}/>
-            <div>testing commit</div>
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="bg-Light_black min-h-[517px] min-w-[454px] rounded-[12px] flex flex-col items-center">
+                     <p className="mt-8 font-bold text-Bright_red text-[28px]">Login</p>
+                     <div className="mt-12 flex flex-col justify-left min-w-[454px] pl-10">
+                        <p className="text-[20px] text-White font-medium">Email</p>
+                        
+                     </div>
+                </div>
+            </div>
         </>
     )
 };
