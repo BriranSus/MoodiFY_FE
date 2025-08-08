44e282b6d52c6ede7de36cf54b4316097186ec7f
BriranSus <alexander.susanto001@binus.ac.id>
2025-08-08 11:47:31 +0700

    Added BE Fetch Logic to Login Page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,24 +1,68 @@
-import InputEmail from "../component/inputEmail";
+import { useEffect, useState } from "react";
+import InputEmail from "../component/InputEmail";
 import InputPassword from "../component/InputPassword";
 import Navbar from "../component/Navbar";
+import API_PATH from "../authentication/API_PATH";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 function LoginPage() {
     const textStyle = "text-[20px] text-White font-medium pt-6";
+    const token = localStorage.getItem("token");
+    const [noLogin, setNoLogin] = useState(false);
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token){
+            setNoLogin(false);
+        }else{
+            setNoLogin(true);
+        }
+    })
+
+    const handleLogin = async() => {
+        setError("")
+        if (!InputEmail || !InputPassword){
+            setError("Please enter both email and password!");
+            return;
+        }
+
+        try{
+            const response = await axios.post(`${API_PATH}/(insert direction api)`, {
+                email: InputEmail,
+                password: InputPassword,
+            });
+
+            const token = response.data.payload.access_token;
+            
+            localStorage.setItem("token", token);
+
+            alert("Login Successful!")
+            navigate("/selection")
+
+        }catch (err){
+            setError("Login failed. Please check your email and password.");
+        }
+    }
 
     return (
         <>
             <Navbar currentPage={"Login"}/>
-            <div className="flex justify-center items-center min-h-screen">
-                <div className="bg-Light_black min-h-[517px] min-w-[454px] rounded-[12px] flex flex-col items-center">
-                     <p className="mt-8 font-bold text-Bright_red text-[28px]">Login</p>
-                     <div className="mt-6 flex flex-col justify-left min-w-[454px] pl-10">
-                        <p className={textStyle}>Email</p>
-                        <InputEmail/>
-                        <p className={textStyle}>Password</p>
-                        <InputPassword/>
-                     </div>
-                </div>
-            </div>
+            {noLogin && (
+                <div className="flex justify-center items-center min-h-screen">
+                    <div className="bg-Light_black min-h-[517px] min-w-[454px] rounded-[12px] flex flex-col items-center">
+                        <p className="mt-8 font-bold text-Bright_red text-[28px]">Login</p>
+                        <div className="mt-6 flex flex-col justify-left min-w-[454px] pl-10">
+                            <p className={textStyle}>Email</p>
+                            <InputEmail/>
+                            <p className={textStyle}>Password</p>
+                            <InputPassword/>
+                        </div>
+                    </div>
+                </div>    
+            )}
+            
         </>
     )
 };
