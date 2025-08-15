2ae48b5d5f64f261d258aa8b1145fb891440ff7a
BriranSus <alexander.susanto001@binus.ac.id>
2025-08-15 10:57:19 +0700

    Fixed Selection Page 2 Navbar

--- src/pages/SelectionPage2.tsx
+++ src/pages/SelectionPage2.tsx
@@ -46,7 +46,6 @@ function SelectionPage2() {
         <>
             <Navbar currentPage={"Selection"}/>
             <div className="overflow-x-hidden">
-                <Navbar currentPage={"Selection"}/>
                 <div className="flex flex-col justify-center min-h-screen min-w-screen items-center pt-36">
                     <h1 className="text-Semi_bright_red font-bold text-[36px]">Find the Perfect Song Based on Your Mood!</h1>   
                     <div className="flex text-[32px] mt-28">
