e2d1c389e1f60851d47bf7be44ed87bf6c4664cc
BriranSus <alexander.susanto001@binus.ac.id>
2025-08-16 20:52:52 +0700

    Added size for Mood Display in Home Page

--- src/component/Mood.tsx
+++ src/component/Mood.tsx
@@ -2,17 +2,17 @@ function Mood(props: { selectedMood: any; }){
     return(
         <>
             {props.selectedMood == "Angry" && (
-                <p className="text-White">
+                <p className="text-White bg-Angry min-h-[531px]">
                     Anger
                 </p>
             )}
             {props.selectedMood== "Happy" && (
-                <p className="text-White">
+                <p className="text-White bg-Happy  min-h-[531px]">
                     Happy
                 </p>
             )}
             {props.selectedMood == "Sad" && (
-                <p className="text-White">
+                <p className="text-White bg-Sad min-h-[531px]">
                     Sad
                 </p>
             )}
